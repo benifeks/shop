8f3da6e03e0312b6081fd4f9a670b7ffb61c5cca
Sergey <benifeks72@gmail.com>
2024-05-17 11:24:26 +0300

    new

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { IProducts } from 'src/models/products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: IProducts;
+  productSubscrition: Subscription;
 
+  public constructor(public route: ActivatedRoute) {}
+  public ngOnInit(): void {
+    this.productSubscrition = this.route.data.subscribe((data: Data) => {
+      this.product = data['data'];
+    });
+  }
 }
